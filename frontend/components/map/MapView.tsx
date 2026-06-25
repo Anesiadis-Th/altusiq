5a0aa71f201724fb2840ae91da353e18c4dd5b28
Anesiadis, Theocharis <xarhsanesiadhs123@gmail.com>
2026-06-25 23:49:49 +0200

    Plane appearance change

--- frontend/components/map/MapView.tsx
+++ frontend/components/map/MapView.tsx
@@ -22,7 +22,7 @@ interface MapViewProps {
   playbackPosition?: PlaybackPosition | null;
 }
 
-const AIRCRAFT_SVG = `<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24"><path d="M21 16V14L13 9V3.5C13 2.67 12.33 2 11.5 2C10.67 2 10 2.67 10 3.5V9L2 14V16L10 13.5V19L8 20.5V22L11.5 21L15 22V20.5L13 19V13.5L21 16Z" fill="#63b3ed"/></svg>`;
+const AIRCRAFT_SVG = `<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24"><path d="M21 16V14L13 9V3.5C13 2.67 12.33 2 11.5 2C10.67 2 10 2.67 10 3.5V9L2 14V16L10 13.5V19L8 20.5V22L11.5 21L15 22V20.5L13 19V13.5L21 16Z" fill="#63b3ed" stroke="#1a202c" stroke-width="1.5" stroke-linejoin="round"/></svg>`;
 
 const rotateExpression: mapboxgl.ExpressionSpecification = [
   "coalesce",
